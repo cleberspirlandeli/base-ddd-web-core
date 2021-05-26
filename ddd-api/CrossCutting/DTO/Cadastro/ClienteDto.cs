47c6ef77f0e2b49a159b3a0c806eb3c1153b527f
Cleber Rezende <simeutenhumsn@hotmail.com>
2021-05-25 21:12:51 -0300

    Correção de bug no startup do projeto

---
+++ ddd-api/CrossCutting/DTO/Cadastro/ClienteDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Common.DTO.Cadastro
+{
+    public class ClienteDto
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Ativo { get; set; }
+    }
+}
